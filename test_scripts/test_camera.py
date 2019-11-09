91c001e4701dc33d9fa3dc1b12d06e79d543128c
Aiste <Austrevicius>
2019-11-09 15:07:52 -0600

    modified script to take pictures

--- test_scripts/test_camera.py
+++ test_scripts/test_camera.py
@@ -1,9 +1,13 @@
 import time
 import picamera
+
+usb_path = "/media/pi/PORTFOLIO"
 camera = picamera.PiCamera()
+
 try:
    camera.start_preview()
-   time.sleep(10)
+   time.sleep(5)
+   camera.capture(usb_path)
    camera.stop_preview()
 finally:
    camera.close()
