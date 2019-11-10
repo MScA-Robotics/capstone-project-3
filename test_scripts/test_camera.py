77dd75a7a04755e58e0ded4f087ebe9f4317b430
Scott Shepard <scott@shepardmail.com>
2019-11-10 05:41:33 +0300

    Don't need to wait so long

--- test_scripts/test_camera.py
+++ test_scripts/test_camera.py
@@ -7,7 +7,7 @@ camera = picamera.PiCamera()
 
 try:
    camera.start_preview()
-   time.sleep(5)
+   time.sleep(2)
    camera.capture(usb_path + "image_{0}.jpg".format(datetime.now().strftime('%m%d%Y%H%M%S')))
    camera.stop_preview()
 finally:
