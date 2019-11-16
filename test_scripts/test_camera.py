d0639ec54edd7c8495b2bff4c4cc7cefe9cc1bf5
Scott Shepard <scott@shepardmail.com>
2019-11-17 00:20:58 +0300

    Changed output file path

--- test_scripts/test_camera.py
+++ test_scripts/test_camera.py
@@ -1,14 +1,15 @@
+
 import time
 import picamera
 from datetime import datetime
 
-usb_path = "/media/pi/PORTFOLIO"
+usb_path = "/media/pi/PORTFOLIO/"
 camera = picamera.PiCamera()
 
 try:
    camera.start_preview()
-   time.sleep(5)
-   camera.capture(usb_path + "image_{0}.jpg".format(datetime.now()))
+   time.sleep(2)
+   camera.capture(usb_path + "image_{0}.jpg".format(datetime.now().strftime('%m%d%Y%H%M%S')))
    camera.stop_preview()
 finally:
    camera.close()
