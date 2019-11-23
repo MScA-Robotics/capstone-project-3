b1ab2963302252f97ac60c053b0e02097ce9ae70
Scott Shepard <scott@shepardmail.com>
2019-11-23 23:32:13 +0300

    Removed preview

--- test_scripts/test_camera.py
+++ test_scripts/test_camera.py
@@ -3,13 +3,10 @@ import time
 import picamera
 from datetime import datetime
 
-usb_path = "/media/pi/PORTFOLIO/"
+usb_path = "/home/pi/Desktop/cone_images/"
 camera = picamera.PiCamera()
 
 try:
-   camera.start_preview()
-   time.sleep(2)
    camera.capture(usb_path + "image_{0}.jpg".format(datetime.now().strftime('%m%d%Y%H%M%S')))
-   camera.stop_preview()
 finally:
    camera.close()
