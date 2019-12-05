fe81af5b452326f1e2a08666c5083c6f53553fc3
Scott Shepard <scott@shepardmail.com>
2019-12-05 10:04:38 +0300

    coneBot class

--- proof_of_concept/proof_of_concept.py
+++ proof_of_concept/proof_of_concept.py
@@ -1,8 +1,59 @@
 from coneDetection import *
+from colorDetection import *
 import pdb
 import os
 
-PIC_PATH = '/home/pi/Desktop/'
+DIR_PATH = '/home/pi/Desktop/cone_images/'
 
-take_picture(PIC_PATH + 'img.jpg')
-# cone = findCone('~/Desktop/img.png')
+class ConeBot
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        
+    def calibrate(self):
+        pic_name = dir_path + 'img.jpg'
+        take_picture(pic_name)
+        boundaries = get_hsv_boundaries(pic_name)
+        self.boundaries = boundaries
+        
+
+
+bot = ConeBot()DIfrom coneDetection import *
+from colorDetection import *
+import pdb
+import os
+
+DIR_PATH = '/home/pi/Desktop/cone_images/'
+
+class ConeBot
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        
+    def calibrate(self):
+        pic_name = dir_path + 'img.jpg'
+        take_picture(pic_name)
+        boundaries = get_hsv_boundaries(pic_name)
+        self.boundaries = boundaries
+        
+from coneDetection import *
+from colorDetection import *
+import pdb
+import os
+
+DIR_PATH = '/home/pi/Desktop/cone_images/'
+
+class ConeBot
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        
+    def calibrate(self):
+        pic_name = dir_path + 'img.jpg'
+        take_picture(pic_name)
+        boundaries = get_hsv_boundaries(pic_name)
+        self.boundaries = boundaries
+        
+
+cone = findCone(colorBounds = boundaries, imageSource='camera')
+print(cone)
