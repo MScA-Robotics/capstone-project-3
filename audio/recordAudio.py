6b32e667f93357c5fac08e354ac50b526910e965
ddobrzynski <dan.dobrzynski@gmail.com>
2020-02-09 00:39:23 +0300

    Functionalize the record chunk

--- audio/recordAudio.py
+++ audio/recordAudio.py
@@ -2,39 +2,44 @@ import pyaudio
 import wave
 from datetime import datetime
 
-form_1 = pyaudio.paInt16 # 16-bit resolution
-chans = 1 # 1 channel
-samp_rate = 44100 # 44.1kHz sampling rate
-chunk = 4096 # 2^12 samples for buffer
-record_secs = 30 # seconds to record
-dev_index = 2 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = 'omnimic_recording_{}.wav'.format(datetime.now().strftime('%m%d%Y%H%M%S')) # name of .wav file
+def record_chunk(record_secs = 5,
+                 chans = 1,
+                 samp_rate = 44100,
+                 chunk = 4096,
+                 dev_index = 0,
+                 wav_output_filename = 'recording_{}.wav'.format(datetime.now().strftime('%m%d%Y%H%M%S'))): # name of .wav file
 
-audio = pyaudio.PyAudio() # create pyaudio instantiation
+    form_1 = pyaudio.paInt16 # 16-bit resolution
 
-# create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+    audio = pyaudio.PyAudio() # create pyaudio instantiation
+
+    # create pyaudio stream
+    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=chunk)
-print("recording")
-frames = []
+    print("recording")
+    frames = []
 
-# loop through stream and append audio chunks to frame array
-for ii in range(0,int((samp_rate/chunk)*record_secs)):
-    data = stream.read(chunk)# exception_on_overflow = False)
-    frames.append(data)
+    # loop through stream and append audio chunks to frame array
+    for ii in range(0,int((samp_rate/chunk)*record_secs)):
+        data = stream.read(chunk,exception_on_overflow = False)
+        frames.append(data)
 
-print("finished recording")
+    print("finished recording")
 
-# stop the stream, close it, and terminate the pyaudio instantiation
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    # stop the stream, close it, and terminate the pyaudio instantiation
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+    # save the audio frames as .wav file
+    wavefile = wave.open(wav_output_filename,'wb')
+    wavefile.setnchannels(chans)
+    wavefile.setsampwidth(audio.get_sample_size(form_1))
+    wavefile.setframerate(samp_rate)
+    wavefile.writeframes(b''.join(frames))
+    wavefile.close()
+
+if __name__ == '__main__':
+
+    record_chunk()
