8ae4ff5611fbb04aa0c253f37435abba3bc65c56
Scott Shepard <scott@shepardmail.com>
2020-02-22 14:54:39 -0600

    Initiate scavbot with image model

--- scav-hunt/scavbot.py
+++ scav-hunt/scavbot.py
@@ -4,3 +4,12 @@ from datetime import datetime
 
 from easygopigo3 import EasyGoPiGo3
 import picamera
+
+import scavcam
+
+class ScavBot:
+
+    def __init__(self, image_model_dir, image_dir):
+        self.image_model = scavcam.ObjectClassificationModel(
+            model_dir = image_model_dir,
+            image_dir = image_dir)
