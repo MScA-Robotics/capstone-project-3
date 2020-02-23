56d802214504d3e3c51f4ed97fe8add409652d9c
Scott Shepard <scott@shepardmail.com>
2020-02-22 21:18:22 -0600

    Instantiate the robot with a navigation driver

--- scav-hunt/scavbot.py
+++ scav-hunt/scavbot.py
@@ -5,11 +5,24 @@ from datetime import datetime
 from easygopigo3 import EasyGoPiGo3
 import picamera
 
-import scavcam
+from scaveye import ObjectClassificationModel
+from scavnav import NavigationControl
 
 class ScavBot:
 
     def __init__(self, image_model_dir, image_dir):
-        self.image_model = scavcam.ObjectClassificationModel(
+        # The bot
+        self.gpg = EasyGoPiGo3() 
+
+        # Bot Controls
+        self.driver = NavigationControl(self.gpg)
+
+        # Image Model
+        self.image_model = ObjectClassificationModel( 
             model_dir = image_model_dir,
             image_dir = image_dir)
+
+        
+if __name__ == '__main__':
+    bot = ScavBot(image_model_dir='Sample_TFlite_model', image_dir='/home/pi/Pictures')
+
