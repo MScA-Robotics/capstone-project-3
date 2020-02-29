fe9ba306bf6de613f87cb5f0a51c9a990eb5f70a
Scott Shepard <scott@shepardmail.com>
2020-02-28 23:32:07 -0500

    Scav Hunt: Working classes in new infra (#10)
    * Scavenger Navigation file for controlling the robot movements
    
    * Instantiate the robot with a navigation driver
    
    * Remove scavnav and put functions in scavbot
    
    * use config params
    
    * Import take_picture as well
    
    * Circumnav needs to use self.gpg for all commands
    
    * Calibrate boundaires and store in a json file
    
    * Move cone code into it's own directory
    
    * Load the boundaries from a json file
    
    * scavbot.py runs and find_cone works with colors
    
    * Establish servo object

---
+++ scav-hunt/config.py
@@ -0,0 +1,7 @@
+
+params = {
+    'radius': 200,
+    'h_spd': 400,
+    'm_spd': 200,
+    'l_spd': 30
+}
--- scav-hunt/scavbot.py
+++ scav-hunt/scavbot.py
@@ -5,11 +5,79 @@ from datetime import datetime
 from easygopigo3 import EasyGoPiGo3
 import picamera
 
-import scavcam
+from scaveye import ObjectClassificationModel, take_picture
+from coneutils import detect
 
 class ScavBot:
 
-    def __init__(self, image_model_dir, image_dir):
-        self.image_model = scavcam.ObjectClassificationModel(
+    def __init__(self, image_model_dir, image_dir, params, boundaries):
+        self.gpg = EasyGoPiGo3()
+        self.dist_sensor = self.gpg.init_distance_sensor()
+        self.servo = self.gpg.init_servo("SERVO1")
+        self.servo.rotate_servo(100)
+        
+        self.params = params
+        self.boundaries = boundaries
+
+        # Image Model
+        self.image_model = ObjectClassificationModel( 
             model_dir = image_model_dir,
             image_dir = image_dir)
+        
+    def find_cone(self, color):
+        bounds = self.boundaries[color]
+        return detect.findCone(bounds)
+        
+    def drive_to_cone(self):
+        # Drive to cone at full bore
+        self.gpg.set_speed(self.params['h_spd'])
+        ob_dist = self.dist_sensor.read_mm()
+        while ob_dist >= self.params['radius']:
+            self.gpg.forward()
+            ob_dist = self.dist_sensor.read_mm()
+            print("Distance Sensor Reading: {} mm ".format(ob_dist))
+        self.gpg.stop()
+
+        # Back away to the exact distance at a slower speed
+        self.gpg.set_speed(self.params['l_spd'])
+        while ob_dist < rad:
+            self.gpg.backward()
+            ob_dist = self.dist_sensor.read_mm()
+            print("Distance Sensor Reading: {} mm ".format(ob_dist))  
+        self.gpg.stop()
+        print("MADE IT!")
+
+    def circum_navigate(self):
+        # Set the speed to medium speed
+        self.gpg.set_speed(self.params['m_spd'])
+        print("I will now cicle the cone at {} mm ".format(self.params['radius']))
+
+        # Circumscibe a circle around the cone
+        # rotate gpg 90 degrees to prep for the orbit
+        self.gpg.turn_degrees(-90)
+
+        # Complete the orbit
+        self.gpg.orbit(180, (2*self.params['radius']/10))
+
+        # Rotate back to facing the cone
+        self.gpg.turn_degrees(90)
+        ob_dist = self.dist_sensor.read_mm()
+        print("The cone is now at: {} mm ".format(ob_dist))
+
+        # Return to a base position
+        print("That was fun... I go home now") 
+        self.gpg.drive_cm(-20,True)
+
+if __name__ == '__main__':
+    import config
+    from coneutils import calibrate
+
+    boundaries_dict = calibrate.load_boundaries('coneutils/boundaries.json')
+
+    bot = ScavBot(
+        image_model_dir='Sample_TFLite_model', 
+        image_dir='/home/pi/Pictures',
+        params=config.params,
+        boundaries = boundaries_dict
+    )
+
